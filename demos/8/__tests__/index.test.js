913260b48e24b07967d5871bfca2f30acc115a45
Chris Hutchinson <chris_hutchinson@me.com>
2019-07-23 20:38:27 +0100

    Fix router test

--- demos/8/__tests__/index.test.js
+++ demos/8/__tests__/index.test.js
@@ -2,18 +2,17 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 
-import App from "../index";
+import Routes from "../index";
 
-const visit = (path, options) =>
+const visit = path =>
   render(
-    <MemoryRouter initialHistoryEntries={[path]}>
-      <App />
-    </MemoryRouter>,
-    options
+    <MemoryRouter initialEntries={[path]}>
+      <Routes />
+    </MemoryRouter>
   );
 
 describe("Demo #8", () => {
-  it("should call the onSubmit prop with the location when the form is submitted", async () => {
+  it("should render the form when the `/form` route is visited", async () => {
     const { getByPlaceholderText } = visit("/form");
 
     getByPlaceholderText("Location");
